0e88261457b0e05fea6eb10f1cf4565cc57f8c25
Oleg <darek2587@gmail.com>
2024-01-28 19:40:00 +0300

    create repository

---
+++ src/test/java/homeWorks/hw1/CalculatorTest.java
@@ -0,0 +1,20 @@
+package homeWorks.hw1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void calculation() {
+    }
+
+    @Test
+    void squareRootExtraction() {
+    }
+
+    @Test
+    void calculatingDiscount() {
+    }
+}

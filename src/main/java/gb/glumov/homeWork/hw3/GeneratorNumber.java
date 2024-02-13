e8417348efe3cb542928852ffdfccee7ec8eea6f
Oleg <darek2587@gmail.com>
2024-02-13 20:10:51 +0300

    create method numberInInterval

--- src/main/java/gb/glumov/homeWork/hw3/GeneratorNumber.java
+++ src/main/java/gb/glumov/homeWork/hw3/GeneratorNumber.java
@@ -14,6 +14,15 @@ public class GeneratorNumber {
 
     //Разработайте и протестируйте метод numberInInterval, который проверяет, попадает ли переданное число в интервал (25;100)
     public static boolean numberInInterval() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+        System.out.println("Рандомное число : " + randomNumber);
+        if (randomNumber > 25 && randomNumber < 100){
+            System.out.println("Число попадает в заданный интервал от 25 до 100");
+        }
+        else {
+            throw new RuntimeException("Ошибка, число не попадает в интервал от 25 до 100");
+        }
         return false;
     }
 

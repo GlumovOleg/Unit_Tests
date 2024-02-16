18bba5bd7d6d53a24add9a01ef5f3bdabd3f267e
Oleg <darek2587@gmail.com>
2024-02-16 18:37:19 +0300

    add hw4

---
+++ src/test/java/gb/glumov/homeWork/hw4/book/BookServiceTest.java
@@ -0,0 +1,16 @@
+package gb.glumov.homeWork.hw4.book;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void findBookById() {
+    }
+
+    @Test
+    void findAllBooks() {
+    }
+}

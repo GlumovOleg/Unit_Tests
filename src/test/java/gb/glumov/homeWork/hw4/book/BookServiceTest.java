56909927f1b667dffe62780a93061f7a8bbe9b6d
Oleg <darek2587@gmail.com>
2024-02-16 20:11:55 +0300

    update test

--- src/test/java/gb/glumov/homeWork/hw4/book/BookServiceTest.java
+++ src/test/java/gb/glumov/homeWork/hw4/book/BookServiceTest.java
@@ -1,16 +1,23 @@
 package gb.glumov.homeWork.hw4.book;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class BookServiceTest {
 
     @Test
-    void findBookById() {
-    }
+    void testGetBookById(){
+        BookRepository bookRepository = mock(BookRepository.class);
 
-    @Test
-    void findAllBooks() {
+        BookService bookService = new BookService(bookRepository);
+
+        when(bookRepository.findById("5")).thenReturn(new Book("5"));
+
+        Book book = bookService.findBookById("5");
+
+        assertEquals("5", book.getId());
+
+        verify(bookRepository, times(1)).findById("5");
     }
 }
